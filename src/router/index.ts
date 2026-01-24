562142a11b387f3cb7d1982069ef91f66c55c512
Landan Fagan <landan@anolar.net>
2026-01-24 14:31:33 -0700

    ~ FormCard color as prop

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,12 @@
 import HomeView from '@/views/HomeView.vue'
+import LobbyView from '@/views/LobbyView.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    { path: '/', component: HomeView }
+    { path: '/', component: HomeView },
+    { path: '/join/:roomCode', component: LobbyView },
   ],
 })
 
