87b514db0dadd510b6ec90911d8905445aa288ea
Landan Fagan <landan@anolar.net>
2026-09-12 19:21:24 -0600

    ~ Make draw/discard smaller

--- src/components/board/DiscardPile.vue
+++ src/components/board/DiscardPile.vue
@@ -1,22 +1,19 @@
 <script setup lang="ts">
-import type { Card } from '@/types/canasta'
-import PlayingCard from './PlayingCard.vue'
+import type { Card } from "@/types/canasta";
+import PlayingCard from "./PlayingCard.vue";
 
 defineProps<{
-  topCard?: Card
-  count: number
-}>()
+  topCard?: Card;
+  count: number;
+}>();
 </script>
 
 <template>
-  <div class="relative aspect-769/1065 w-[calc(var(--card-base-width)*var(--card-scale,1))]">
+  <div class="relative aspect-769/1065 w-[calc(var(--card-base-width)*var(--card-scale,1)*0.75)]">
     <PlayingCard v-if="topCard" :card="topCard" />
-    <div
-      v-else
-      class="absolute inset-0 rounded-2xl border-2 border-dashed border-card-white/40"
-    />
+    <div v-else class="absolute inset-0 rounded-2xl border-2 border-dashed border-card-white/40" />
     <span class="absolute -bottom-6 left-1/2 -translate-x-1/2 font-rs-bold text-card-white">{{
       count
-    }}</span>
+      }}</span>
   </div>
 </template>
